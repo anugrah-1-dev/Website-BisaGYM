4b49212f814aaaf17c1539740cfdbe09678acf5f
anugrah-1-dev <tejomaliki1@gmail.com>
2026-08-14 22:43:05 +0700

    fix

--- database/migrations/2026_07_29_170951_create_activity_logs_table.php
+++ database/migrations/2026_07_29_170951_create_activity_logs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('activity_logs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
             $table->string('action'); // CREATE, UPDATE, DELETE, dll
             $table->string('module'); // Manajemen User, Paket Gym, dll
             $table->text('description'); // Deskripsi spesifik
